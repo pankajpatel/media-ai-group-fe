02d98fb29ac7a81d9aef9e3fad514f7b24a096bc
Pankaj Patel <p.patel@holidaypirates.com>
2017-11-18 21:25:45 +0100

    dash

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,16 +10,43 @@ export class DashboardComponent {
   public charts1Data: any;
   public graphData: any;
   constructor(private httpRequestService: HttpRequestService) {
-
+    const labels = [
+      'disgust',
+      'anger',
+      'contempt',
+      'engagement',
+      'fear',
+      'joy',
+      'sadness',
+      'surprise',
+      'valence',
+      'attention',
+    ];
     // console.log(data);
-    this.graphData = {
-      labels: [0, 20, 40, 60, 80, 100, 120, 140, 160, 180]
-    };
-    this.charts1Data = [{
-      label: 'Happiness',
-      data: [25, 51, 30, 81, 56, 25, 40, 50, 89, 10],
-      fill: false,
-    }]
+    this.graphData = { labels };
+
+    this.httpRequestService.getEmotions('api/videos/1/emotions/stats')
+      .first() // only gets fired once
+      .subscribe(data => {
+        // const chartDataAllEmotions = this.chartDataParsingService.parseEmotionsData(data, this.keys());
+        // this.lineChartDatasets = this.keys().map((chartName: string) => {
+        //   const chartData = this.chartDataParsingService.getDefault()[chartName];
+        //   chartData.data = chartDataAllEmotions[chartName];
+        //   return chartData;
+        // });
+        // console.log('this.lineChartDatasets', this.lineChartDatasets);
+        const d = Object.keys(data).reduce((acc, key) => {
+          acc.push(data[key]);
+          return acc;
+        }, []);
+        this.charts1Data = [{
+          label: 'Video Stats',
+          data: d,
+          fill: false,
+        }]
+        console.log(data);
+      });
+
   }
 
 }
