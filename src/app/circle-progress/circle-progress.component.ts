7fdf7a74bd0857fbf4dbc737e50670d1004e59fb
Ayoub <hajjem.ayoub@gmail.com>
2017-11-19 10:01:27 +0100

    doughnut showing ugly

--- src/app/circle-progress/circle-progress.component.ts
+++ src/app/circle-progress/circle-progress.component.ts
@@ -7,26 +7,29 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CircleProgressComponent implements OnInit {
 
-  @Input() value: number;
-  @Input() suffix: string;
-  @Input() primaryColor: string;
-  @Input() secondaryColor: string;
-  @Input() clockWise: Boolean;
-  public dataset: Array<any>;
+  // @Input() value: number;
+  // @Input() suffix: string;
+  // @Input() primaryColor: string;
+  // @Input() secondaryColor: string;
+  // @Input() clockWise: Boolean;
+  public data: Array<any>;
+  public labels: Array<any>;
   public colors: Array<any>;
   ngOnInit() {
-    this.clockWise = this.clockWise || true;
-    this.primaryColor = this.primaryColor || "rgba(36, 112, 250, 1)";
-    this.secondaryColor = this.secondaryColor || "rgba(0, 0, 0, 0)";
-    this.value = Number(this.value);
-    this.dataset = [{
-      data: this.clockWise ? [this.value, (100 - this.value)] : [(100 - this.value), this.value],
-      backgroundColor: [
-        this.primaryColor,
-        this.secondaryColor,
-      ]
-    }];
-    console.log(this.dataset)
+    // this.clockWise = this.clockWise || true;
+    // this.primaryColor = this.primaryColor || "rgba(36, 112, 250, 1)";
+    // this.secondaryColor = this.secondaryColor || "rgba(0, 0, 0, 0)";
+    // this.value = Number(this.value);
+    // this.data = this.clockWise ? [this.value, (100 - this.value)] : [(100 - this.value), this.value];
+    this.data = [30, 100];
+    this.labels = ['dsf', 'sjjj'];
+    // this.dataset = [{
+    //   backgroundColor: [
+    //     this.primaryColor,
+    //     this.secondaryColor,
+    //   ]
+    // }];
+    // console.log(this.dataset)
   }
 
 }
