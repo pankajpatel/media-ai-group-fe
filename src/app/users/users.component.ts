4d32ac426d6cb2da55239e4dbfb73afa0adfa45e
Pankaj Patel <p.patel@holidaypirates.com>
2017-11-19 12:41:25 +0100

    table

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { HttpRequestService } from './../service/http-request.service';
 import { Component, OnInit } from '@angular/core';
+import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
 
 @Component({
   selector: 'users',
@@ -8,13 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
   public users: any;
+  private alive: boolean;
   constructor(private httpRequestService: HttpRequestService) {
+    this.alive = true;
     this.httpRequestService.getEmotions('api/videos/1/users')
       .first() // only gets fired once
       .subscribe(data => {
-        console.log('{}{}{}{}{}{}{}{}}');
-        console.log(data);
+        this.users = new Array(Number(data.users_watching));
       })
+    IntervalObservable.create(1000)
+      .takeWhile(() => this.alive) // only fires when component is alive
+      .subscribe(() => {
+        this.httpRequestService.getEmotions('api/videos/1/users')
+          .subscribe(data => {
+            this.users = new Array(Number(data.users_watching));
+          })
+      });
   }
 
   ngOnInit() {
